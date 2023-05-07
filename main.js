1989e3e99a6ffd83a67a186733a01cc7fe2cce74
Agam Vito Asyhari <81339836+hellogirl-dev@users.noreply.github.com>
2023-05-07 22:11:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// console.log(document.title)
+// console.log(document.body)
+
+// const body = document.body
+// body.append("Hello World")
+
+// const namaSaya = document.createElement('h1')
+// namaSaya.textContent = "Selamat Datang"
+
+// const p = document.createElement('p')
+// p.innerHTML = "Nama saya Agam Vito Asyhari NIM 202031248"
+
+// let saldoAwal = 40000
+// let saldoTambahan = 90000
+// let saldoAkhir = saldoAwal + saldoTambahan
+
+// body.append(namaSaya)
+// body.append(p)
+// body.append(saldoAkhir)
